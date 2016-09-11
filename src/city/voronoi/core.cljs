4b65090e417728bd3d20fc750dd832a6689d9706
zerosalife <zerosalife@gmail.com>
2016-09-11 15:55:36 -0500

    Add voronoi diagram construction function and test
    - Something seems to be wrong.  Going to try to plot expected and
      observed results to debug.

--- src/city/voronoi/core.cljs
+++ src/city/voronoi/core.cljs
@@ -1,5 +1,6 @@
 (ns city.voronoi.core
-  (:require [city.voronoi.point :as point]))
+  (:require [city.voronoi.point :as point]
+            [city.voronoi.delaunay-triangulation :as delaunay]))
 
 (defn center-of-triangle [[{x1 :x y1 :y} {x2 :x y2 :y} {x3 :x y3 :y}]]
   (point/Point. (/ (+ x1 x2 x3) 3)
@@ -17,3 +18,24 @@
              (if next-triangle
                (sort-triangles (conj sorted next-triangle) (remove #{next-triangle} triangles))
                sorted)))))
+
+(defn polygon-for-point [triangles point]
+  (let [neighboring-triangles (filter (fn [[a b c]] (or (= point a) (= point b) (= point c))) triangles)]
+    (map center-of-triangle (sort-triangles neighboring-triangles))))
+
+(defn loopify [polygon]
+  (let [polygon (mapcat (fn [x] [x x]) polygon)
+        polygon (concat (rest polygon) [(first polygon)])]
+    polygon))
+
+(defn diagram [points]
+  (let [points (map
+                (fn [p] (point/Point. (float (:x p)) (float (:y p)))) points)
+        cells (map (partial polygon-for-point (:triangles (delaunay/triangulate points))) points)]
+    {:points points
+     :cells cells
+     :edges (->> cells
+                 (map loopify)
+                 (mapcat (partial partition 2))
+                 (map (partial sort-by :x))
+                 distinct)}))
