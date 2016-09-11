f524f5aac754b347b582a62151d641d133e7c86f
zerosalife <zerosalife@gmail.com>
2016-09-11 15:02:56 -0500

    Test finding center of triangle

--- src/city/voronoi/core.cljs
+++ src/city/voronoi/core.cljs
@@ -1,4 +1,6 @@
-(ns city.voronoi.core)
+(ns city.voronoi.core
+  (:require [city.voronoi.point :as point]))
 
-(defn center-of-triangle [_]
-  [10 10])
+(defn center-of-triangle [[{x1 :x y1 :y} {x2 :x y2 :y} {x3 :x y3 :y}]]
+  (point/Point. (/ (+ x1 x2 x3) 3)
+                (/ (+ y1 y2 y3) 3)))
--- test/city/voronoi/core_test.cljs
+++ test/city/voronoi/core_test.cljs
@@ -1,7 +1,10 @@
 (ns city.voronoi.core-test
   (:require [cljs.test :refer-macros [deftest is testing]]
-            [city.voronoi.core :as core]))
+            [city.voronoi.core :as core]
+            [city.voronoi.point :as point]))
+
+(def p point/Point)
 
 (deftest center-of-triangle-test
-  (is (= [10 10]
-         (core/center-of-triangle [[30 0] [0 0] [0 30]]))))
+  (is (= (p. 10 10)
+         (core/center-of-triangle [(p. 30 0) (p. 0 0) (p. 0 30)]))))
