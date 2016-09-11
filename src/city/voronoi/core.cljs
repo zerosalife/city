ba55de3e48eee76e411ed6a969bfca228b0e2dff
zerosalife <zerosalife@gmail.com>
2016-09-11 15:14:35 -0500

    Add sort triangle and test

--- src/city/voronoi/core.cljs
+++ src/city/voronoi/core.cljs
@@ -4,3 +4,16 @@
 (defn center-of-triangle [[{x1 :x y1 :y} {x2 :x y2 :y} {x3 :x y3 :y}]]
   (point/Point. (/ (+ x1 x2 x3) 3)
                 (/ (+ y1 y2 y3) 3)))
+
+(defn sort-triangles
+  ([triangles] (sort-triangles [] triangles))
+  ([sorted triangles]
+   (cond
+     (empty? triangles) sorted
+     (empty? sorted) (sort-triangles [(first triangles)] (rest triangles))
+     :else (let [previous-corners (set (last sorted))
+                 adjacent? (fn [triangle] (= (count (clojure.set/intersection previous-corners (set triangle))) 2))
+                 next-triangle (first (filter adjacent? triangles))]
+             (if next-triangle
+               (sort-triangles (conj sorted next-triangle) (remove #{next-triangle} triangles))
+               sorted)))))
--- test/city/voronoi/core_test.cljs
+++ test/city/voronoi/core_test.cljs
@@ -8,3 +8,11 @@
 (deftest center-of-triangle-test
   (is (= (p. 10 10)
          (core/center-of-triangle [(p. 30 0) (p. 0 0) (p. 0 30)]))))
+
+(deftest sort-triangles-test
+  (is (= [[(p. 0 0) (p. 5 5) (p. 10 0)]
+          [(p. 10 0) (p. 5 5) (p. 5 -5)]
+          [(p. 5 -5) (p. 5 5) (p. 2 2)]]
+         (core/sort-triangles [[(p. 0 0) (p. 5 5) (p. 10 0)]
+                               [(p. 5 -5) (p. 5 5) (p. 2 2)]
+                               [(p. 10 0) (p. 5 5) (p. 5 -5)]]))))
